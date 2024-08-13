fe76c8243d245f79a67168295451d1c4640555e3
Alexandr <you@example.com>
2024-08-13 14:00:20 +0300

    error in async

--- src/redux/slices/bookSlice.jsx
+++ src/redux/slices/bookSlice.jsx
@@ -1,14 +1,20 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 import createBookWithID from "../../utils/createBookID"
+import { setError } from "./errorSlice"
 
 const initialState = []
 
 export const fetchBook = createAsyncThunk(
   'book/fetchBook',
-  async () => {
-    const res = await axios.get("http://localhost:4000/random-book")
-    return res.data
+  async (url, thunkAPI) => {
+    try {   
+      const res = await axios.get(url)
+      return res.data
+    } catch (error) {
+      thunkAPI.dispatch(setError(error.message))
+      throw error
+    }
   }
 )
 
